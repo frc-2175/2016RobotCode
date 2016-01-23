365451b1f22f187414b8c6b787ccdec39d629533
Noah Visness <nvisness@gmail.com>
2016-01-22 20:50:04 -0600

    Delete unnecessary subsystem and fix typo

--- src/org/usfirst/frc2175/subsystem/shooter/CatapultShooterSubsystem.java
+++ src/org/usfirst/frc2175/subsystem/shooter/CatapultShooterSubsystem.java
@@ -2,7 +2,7 @@ package org.usfirst.frc2175.subsystem.shooter;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class CatapaultShooterSubsystem extends Subsystem {
+public class CatapultShooterSubsystem extends Subsystem {
     @Override
     protected void initDefaultCommand() {
         // TODO setDefaultCommand(new TheCommandToUse());
