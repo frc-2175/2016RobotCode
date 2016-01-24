9687a51dd8213195f18f7ce89052020c360362b7
Jeff Jensen <jjensen@apache.org>
2016-01-24 09:37:56 -0600

    Update command and subsystem package docs to note instance usage.

--- src/org/usfirst/frc2175/command/package-info.java
+++ src/org/usfirst/frc2175/command/package-info.java
@@ -1,4 +1,7 @@
 /**
  * This package and its subpackages contain the command classes.
+ *
+ * Commands are instantiated where needed, as different uses may required
+ * different command configurations (e.g. teleop vs auton).
  */
 package org.usfirst.frc2175.command;
--- src/org/usfirst/frc2175/subsystem/package-info.java
+++ src/org/usfirst/frc2175/subsystem/package-info.java
@@ -1,5 +1,8 @@
 /**
  * This package and its subpackages contain the major components of the robot
  * classes (subsystems).
+ *
+ * Subsystems are instantiated once and held in
+ * {@link org.usfirst.frc2175.subsystem.RobotSubsystems}.
  */
 package org.usfirst.frc2175.subsystem;
