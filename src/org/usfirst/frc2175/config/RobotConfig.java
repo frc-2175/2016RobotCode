5198d086b872ee4a66c072dfa822b98a1bb36d5f
Jeff Jensen <jjensen@apache.org>
2016-01-23 23:05:04 -0600

    Add remaining currently known configs to RobotConfig.

--- src/org/usfirst/frc2175/config/RobotConfig.java
+++ src/org/usfirst/frc2175/config/RobotConfig.java
@@ -4,7 +4,17 @@ package org.usfirst.frc2175.config;
  * Robot configuration values.
  */
 public class RobotConfig {
+    private final GamepadConfig gamepadConfig = new GamepadConfig();
+    private final JoysticksConfig joystickConfig = new JoysticksConfig();
     private final WiringConfig wiringConfig = new WiringConfig();
+
+    public GamepadConfig getGamepadConfig() {
+        return gamepadConfig;
+    }
+
+    public JoysticksConfig getJoysticksConfig() {
+        return joystickConfig;
+    }
 
     public WiringConfig getWiringConfig() {
         return wiringConfig;
