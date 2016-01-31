039f82a57fe673f2c28366360016c85266b78c8d
Noah Visness <nvisness@gmail.com>
2016-01-30 20:34:04 -0600

    Add commands to run main and side dream intake belts

---
+++ src/org/usfirst/frc2175/command/single/RunDreamIntakeSideBeltAtSpeed.java
@@ -0,0 +1,54 @@
+package org.usfirst.frc2175.command.single;
+
+import org.usfirst.frc2175.driverstation.DriverStation;
+import org.usfirst.frc2175.subsystem.RobotSubsystems;
+import org.usfirst.frc2175.subsystem.intake.DreamIntakeSubsystem;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class RunDreamIntakeSideBeltAtSpeed extends Command {
+
+    double speed;
+    DreamIntakeSubsystem dreamIntakeSubsystem;
+
+    public RunDreamIntakeSideBeltAtSpeed(DriverStation driverStation,
+            RobotSubsystems robotSubsystems, double speed) {
+        dreamIntakeSubsystem = robotSubsystems.getDreamIntakeSubsystem();
+        speed = this.speed;
+
+        requires(dreamIntakeSubsystem);
+    }
+
+    // Called just before this Command runs the first time
+    @Override
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    protected void execute() {
+        dreamIntakeSubsystem.setSideBeltSpeed(speed);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    @Override
+    protected void end() {
+        dreamIntakeSubsystem.setSideBeltSpeed(0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    @Override
+    protected void interrupted() {
+        end();
+    }
+}
