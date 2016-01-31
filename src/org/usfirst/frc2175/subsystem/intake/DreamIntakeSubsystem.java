8b2614f9c385761952ad6f24e357c9a6d43e65ba
Noah Visness <nvisness@gmail.com>
2016-01-30 20:33:36 -0600

    Add wiring config for dream intake talons

--- src/org/usfirst/frc2175/subsystem/intake/DreamIntakeSubsystem.java
+++ src/org/usfirst/frc2175/subsystem/intake/DreamIntakeSubsystem.java
@@ -3,25 +3,32 @@ package org.usfirst.frc2175.subsystem.intake;
 import org.usfirst.frc2175.config.RobotConfig;
 import org.usfirst.frc2175.subsystem.BaseSubsystem;
 
+import edu.wpi.first.wpilibj.Talon;
+
 public class DreamIntakeSubsystem extends BaseSubsystem {
-    // TODO add instance variables for robot components
-    // (Talons, Solenoids, etc.)
+    private Talon dreamIntakeSideBeltTalon;
+    private Talon dreamIntakeMainBeltTalon;
+    private Talon dreamIntakeLiftTalon;
 
     public DreamIntakeSubsystem(RobotConfig robotConfig) {
-        // TODO use values from robotConfig to set instance variables above
-        // Then you can use those variables in the methods below!
+        dreamIntakeSideBeltTalon =
+                robotConfig.getWiringConfig().getDreamIntakeSideBeltTalon();
+        dreamIntakeMainBeltTalon =
+                robotConfig.getWiringConfig().getDreamIntakeMainBeltTalon();
+        dreamIntakeLiftTalon =
+                robotConfig.getWiringConfig().getDreamIntakeLiftTalon();
     }
 
     public void setMainBeltSpeed(double speed) {
-        // TODO Fill in
+        dreamIntakeMainBeltTalon.set(speed);
     }
 
     public void setSideBeltSpeed(double speed) {
-        // TODO Fill in
+        dreamIntakeSideBeltTalon.set(speed);
     }
 
-    public void setPosition(boolean out) {
-        // TODO Fill in
+    public void setLiftSpeed(double speed) {
+        dreamIntakeLiftTalon.set(speed);
     }
 
     public boolean isCompletelyOut() {
