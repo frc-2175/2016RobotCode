e5a964cfcd58ce2b09ae3049e980544cdcb1e72a
Max Haland <max@haland.org>
2016-01-31 16:25:20 -0600

    Add Javadoc

--- src/org/usfirst/frc2175/util/HighestArrayIndexFinder.java
+++ src/org/usfirst/frc2175/util/HighestArrayIndexFinder.java
@@ -1,6 +1,20 @@
 package org.usfirst.frc2175.util;
 
+/**
+ *
+ * @author Max Haland
+ *
+ */
+
 public class HighestArrayIndexFinder {
+
+    /**
+     * Determines the index of the largest value in an array.
+     *
+     * @param array
+     *            Array to use
+     * @return Index of the largest value in the array
+     */
 
     public int determineLargestArrayItemIndex(double[] array) {
         int numberOfItems = array.length;
