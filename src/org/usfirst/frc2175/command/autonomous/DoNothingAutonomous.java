78a5d94ef962e89e314cee5204772c4ed6ab080c
Kevin La Casse <wolfklacasse@gmail.com>
2016-02-06 16:11:13 -0600

    Make a do nothing autonomous

---
+++ src/org/usfirst/frc2175/command/autonomous/DoNothingAutonomous.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc2175.command.autonomous;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class DoNothingAutonomous extends CommandGroup {
+
+    public DoNothingAutonomous() {
+
+    }
+}
