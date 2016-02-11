408bbdf3375906dd2cc3d90c40014061c5a2824a
Kevin La Casse <wolfklacasse@gmail.com>
2016-02-10 20:52:04 -0600

    Make vision processing config test. Do not know if it works

---
+++ test/org/usfirst/frc2175/config/VisionProcessingConfigTest.java
@@ -0,0 +1,30 @@
+package org.usfirst.frc2175.config;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.usfirst.frc2175.TestBase;
+import org.usfirst.frc2175.util.HighestArrayIndexFinder;
+
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import mockit.Mocked;
+
+public class VisionProcessingConfigTest extends TestBase {
+    @Mocked
+    private NetworkTable networkTable;
+    @Mocked
+    private HighestArrayIndexFinder highestArrayIndexFinder;
+
+    @Ignore("Do not know if it works or not.")
+    @Test
+    public void testVisionProcessingConfig_Competition() {
+        BaseConfig.setPropertyFileDir(PROPERTY_FILE_DIR_SRC_COMPETITION);
+        VisionProcessingConfig sut = new VisionProcessingConfig();
+    }
+
+    @Ignore("Do not know if it works or not.")
+    @Test
+    public void testVisionProcessingConfig_Practice() {
+        BaseConfig.setPropertyFileDir(PROPERTY_FILE_DIR_SRC_PRACTICE);
+        VisionProcessingConfig sut = new VisionProcessingConfig();
+    }
+}
