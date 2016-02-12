b320e5f051b31257867129df5c75d2f918b520ab
Joshua Jacobs <joshua.jacobs126@gmail.com>
2016-02-11 20:54:44 -0600

    Made two new empty commands for auton. Labeled with TODOs.

---
+++ src/org/usfirst/frc2175/command/single/DriveInches.java
@@ -0,0 +1,38 @@
+package org.usfirst.frc2175.command.single;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveInches extends Command {
+
+    public DriveInches(double distance) {
+        // TODO Fill In
+    }
+
+    @Override
+    protected void initialize() {
+    }
+
+    @Override
+    protected void execute() {
+        // TODO Fill In
+    }
+
+    @Override
+    protected boolean isFinished() {
+        // TODO Fill In
+        return false;
+    }
+
+    @Override
+    protected void end() {
+        // TODO Fill in
+    }
+
+    @Override
+    protected void interrupted() {
+        end();
+    }
+}
---
+++ src/org/usfirst/frc2175/command/single/TurnDegrees.java
@@ -0,0 +1,38 @@
+package org.usfirst.frc2175.command.single;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class TurnDegrees extends Command {
+
+    public TurnDegrees(double degrees) {
+
+    }
+
+    @Override
+    protected void initialize() {
+    }
+
+    @Override
+    protected void execute() {
+        // TODO Fill in
+    }
+
+    @Override
+    protected boolean isFinished() {
+        // TODO Fill in
+        return false;
+    }
+
+    @Override
+    protected void end() {
+        // TODO Fill in
+    }
+
+    @Override
+    protected void interrupted() {
+        end();
+    }
+}
