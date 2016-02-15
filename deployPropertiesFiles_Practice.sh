dc9572c219da9cec9c43ad14737f3d26a6db697c
Max Haland <max@haland.org>
2016-02-15 16:23:26 -0600

    Correct roboRIO username in property deploy files

--- deployPropertiesFiles_Practice.sh
+++ deployPropertiesFiles_Practice.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-scp src/properties/practicebot/gamepad.properties src/properties/practicebot/intake.properties src/properties/practicebot/joysticks.properties src/properties/practicebot/vision.properties src/properties/practicebot/wiring.properties src/properties/practicebot/ControlLoopConstants.properties root@roborio-2175-frc.local:/home/lvuser
+scp src/properties/practicebot/gamepad.properties src/properties/practicebot/intake.properties src/properties/practicebot/joysticks.properties src/properties/practicebot/vision.properties src/properties/practicebot/wiring.properties src/properties/practicebot/ControlLoopConstants.properties admin@roborio-2175-frc.local:/home/lvuser
