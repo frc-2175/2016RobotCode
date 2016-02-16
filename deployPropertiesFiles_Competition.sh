2d0ba9888915bed9767a1f67cc40d0c374f232f0
Max Haland <max@haland.org>
2016-02-15 22:01:12 -0600

    Change property deploy scripts to restart robot code

--- deployPropertiesFiles_Competition.sh
+++ deployPropertiesFiles_Competition.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 scp src/properties/competitionbot/catapultShooter.properties src/properties/competitionbot/gamepad.properties src/properties/competitionbot/intake.properties src/properties/competitionbot/joysticks.properties src/properties/competitionbot/vision.properties src/properties/competitionbot/wiring.properties src/properties/competitionbot/ControlLoopConstants.properties admin@roborio-2175-frc.local:/home/lvuser
+ssh admin@roborio-2175-frc.local "killall -q netconsole-host || :"
