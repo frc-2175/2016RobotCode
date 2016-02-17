c53abf90d54d14db4ceae103f2cd93d9da39e22d
Max Haland <max@haland.org>
2016-02-16 20:44:42 -0600

    Update property deploy script for manipulaotr

--- deployPropertiesFiles_Competition.sh
+++ deployPropertiesFiles_Competition.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-scp src/properties/competitionbot/catapultShooter.properties src/properties/competitionbot/gamepad.properties src/properties/competitionbot/intake.properties src/properties/competitionbot/joysticks.properties src/properties/competitionbot/vision.properties src/properties/competitionbot/wiring.properties src/properties/competitionbot/ControlLoopConstants.properties admin@roborio-2175-frc.local:/home/lvuser
+# First transfer over the files
+scp src/properties/competitionbot/manipulator.properties src/properties/competitionbot/catapultShooter.properties src/properties/competitionbot/gamepad.properties src/properties/competitionbot/intake.properties src/properties/competitionbot/joysticks.properties src/properties/competitionbot/vision.properties src/properties/competitionbot/wiring.properties src/properties/competitionbot/ControlLoopConstants.properties admin@roborio-2175-frc.local:/home/lvuser
+
+# Then restart robot code
 ssh admin@roborio-2175-frc.local "killall -q netconsole-host || :"
