8e7e6f959843d7f9c6eeb09bb992c2ffb4843602
Max Haland <max@haland.org>
2016-02-17 23:39:26 -0600

    Update deploy scripts for new files

--- deployPropertiesFiles_Practice.sh
+++ deployPropertiesFiles_Practice.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-scp src/properties/practicebot/catapultShooter.properties src/properties/practicebot/gamepad.properties src/properties/practicebot/intake.properties src/properties/practicebot/joysticks.properties src/properties/practicebot/vision.properties src/properties/practicebot/wiring.properties src/properties/practicebot/ControlLoopConstants.properties admin@roborio-2175-frc.local:/home/lvuser
+scp src/properties/practicebot/powertrain.properties src/properties/practicebot/catapultShooter.properties src/properties/practicebot/gamepad.properties src/properties/practicebot/intake.properties src/properties/practicebot/joysticks.properties src/properties/practicebot/vision.properties src/properties/practicebot/wiring.properties src/properties/practicebot/ControlLoopConstants.properties admin@roborio-2175-frc.local:/home/lvuser
 ssh admin@roborio-2175-frc.local "killall -q netconsole-host || :"
