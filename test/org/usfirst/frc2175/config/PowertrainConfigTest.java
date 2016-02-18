ddadf1e7a0b0b281aa060a2ab122177533cf9b69
Max Haland <max@haland.org>
2016-02-18 11:12:55 -0600

    Make test for PowertrainConfig

---
+++ test/org/usfirst/frc2175/config/PowertrainConfigTest.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc2175.config;
+
+import org.junit.Test;
+import org.usfirst.frc2175.TestBase;
+
+public class PowertrainConfigTest extends TestBase {
+    @Test
+    public void testPowertrainConfig_Competition()
+            throws IllegalArgumentException, IllegalAccessException {
+        BaseConfig.setPropertyFileDir(PROPERTY_FILE_DIR_SRC_COMPETITION);
+        PowertrainConfig sut = new PowertrainConfig();
+        assertDoublesNotZero(sut);
+    }
+
+    @Test
+    public void testPowertrainConfig_Practice()
+            throws IllegalArgumentException, IllegalAccessException {
+        BaseConfig.setPropertyFileDir(PROPERTY_FILE_DIR_SRC_PRACTICE);
+        PowertrainConfig sut = new PowertrainConfig();
+        assertDoublesNotZero(sut);
+    }
+}
