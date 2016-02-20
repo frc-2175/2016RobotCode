cd01c3db5a6f41b2f32006f5351eb8479cbb60db
Ben Visness <bvisness@gmail.com>
2016-02-19 20:20:40 -0600

    Fix error so tests pass

--- src/org/usfirst/frc2175/command/autonomous/CrossShootLowBarAutonomous.java
+++ src/org/usfirst/frc2175/command/autonomous/CrossShootLowBarAutonomous.java
@@ -2,6 +2,7 @@ package org.usfirst.frc2175.command.autonomous;
 
 import org.usfirst.frc2175.command.group.TurnToFaceGoalAndShootGroup;
 import org.usfirst.frc2175.command.single.DriveInches;
+import org.usfirst.frc2175.pid.RobotControllers;
 import org.usfirst.frc2175.subsystem.RobotSubsystems;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -11,9 +12,11 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class CrossShootLowBarAutonomous extends CommandGroup {
 
-    public CrossShootLowBarAutonomous(RobotSubsystems robotSubsystems) {
+    public CrossShootLowBarAutonomous(RobotSubsystems robotSubsystems,
+            RobotControllers robotControllers) {
         // TO-DO Refine numbers
         addSequential(new DriveInches(robotSubsystems, 24));
-        addSequential(new TurnToFaceGoalAndShootGroup(robotSubsystems));
+        addSequential(new TurnToFaceGoalAndShootGroup(robotSubsystems,
+                robotControllers));
     }
 }
