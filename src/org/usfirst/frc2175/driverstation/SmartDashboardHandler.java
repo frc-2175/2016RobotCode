66a09ed3062c357c9deccbdcefb185f10207f87b
Jeff Jensen <jjensen@apache.org>
2016-02-20 00:25:58 -0600

    Add log message to constructor

--- src/org/usfirst/frc2175/driverstation/SmartDashboardHandler.java
+++ src/org/usfirst/frc2175/driverstation/SmartDashboardHandler.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc2175.driverstation;
 
+import java.util.logging.Logger;
+
 import org.usfirst.frc2175.command.autonomous.CrossChevalDeFriseAutonomous;
 import org.usfirst.frc2175.command.autonomous.CrossLowBarAutonomous;
 import org.usfirst.frc2175.command.autonomous.CrossMoatAutonomous;
@@ -14,10 +16,14 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class SmartDashboardHandler {
+    private final Logger log = Logger.getLogger(getClass().getName());
+
     private SendableChooser autonChooser;
     private RobotSubsystems robotSubsystems;
 
     public SmartDashboardHandler(RobotSubsystems robotSubsystems) {
+        log.info("Configuring class=" + getClass());
+
         this.robotSubsystems = robotSubsystems;
 
         makeAutonChooser();
