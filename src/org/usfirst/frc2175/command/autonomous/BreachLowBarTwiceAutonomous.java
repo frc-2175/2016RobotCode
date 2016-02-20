7f681a9186e0088169c5957c66110b4f460c1b02
Megan Dalldorf <megandalldorf@mnmsa.org>
2016-02-20 13:24:57 -0600

    Added cross the low bar twice for auton.

---
+++ src/org/usfirst/frc2175/command/autonomous/BreachLowBarTwiceAutonomous.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc2175.command.autonomous;
+
+import org.usfirst.frc2175.command.single.DriveInches;
+import org.usfirst.frc2175.subsystem.RobotSubsystems;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class BreachLowBarTwiceAutonomous {
+	
+	public class BreachPortcullisAutonomous  extends CommandGroup{
+		
+
+	    public BreachPortcullisAutonomous(RobotSubsystems robotSubsystems) {
+	        addSequential(
+	        		//TO-DO Fix number of inches as needed
+	                new DriveInches(robotSubsystems, 24));
+	        		new DriveInches(robotSubsystems, -24);
+	        		new DriveInches(robotSubsystems, 8);
+
+	    }
+	}
+}
