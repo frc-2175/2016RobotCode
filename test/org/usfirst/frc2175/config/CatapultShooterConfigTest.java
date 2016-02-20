de47effdff4474170d9a2984ed3004541ccf84de
Jeff Jensen <jjensen@apache.org>
2016-02-19 22:24:09 -0600

    Add test for CatapultShooterConfig

---
+++ test/org/usfirst/frc2175/config/CatapultShooterConfigTest.java
@@ -0,0 +1,14 @@
+package org.usfirst.frc2175.config;
+
+import org.junit.Test;
+import org.usfirst.frc2175.TestBase;
+
+public class CatapultShooterConfigTest extends TestBase {
+    @Test
+    public void testJoystickConfig_Competition()
+            throws IllegalArgumentException, IllegalAccessException {
+        BaseConfig.setPropertyFileDir(PROPERTY_FILE_DIR_SRC_COMPETITION);
+        CatapultShooterConfig sut = new CatapultShooterConfig();
+        assertDoublesNotZero(sut);
+    }
+}
