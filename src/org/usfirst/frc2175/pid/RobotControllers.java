69a3563aa4697d244256447f559fc8a79abed95e
Ben Visness <bvisness@gmail.com>
2016-02-19 20:08:24 -0600

    Add GyroTurnPIDController to RobotControllers

--- src/org/usfirst/frc2175/pid/RobotControllers.java
+++ src/org/usfirst/frc2175/pid/RobotControllers.java
@@ -7,16 +7,23 @@ public class RobotControllers {
     private final RobotConfig robotConfig;
 
     private final VisionTurnPIDController visionTurnPIDController;
+    private final GyroTurnPIDController gyroTurnPIDController;
 
     public RobotControllers(RobotConfig robotConfig,
             RobotSubsystems robotSubsystems) {
         this.robotConfig = robotConfig;
         this.visionTurnPIDController = new VisionTurnPIDController(
                 robotSubsystems, robotConfig);
+        this.gyroTurnPIDController = new GyroTurnPIDController(robotSubsystems,
+                robotConfig);
     }
 
     public VisionTurnPIDController getVisionTurnPIDController() {
         return visionTurnPIDController;
+    }
+
+    public GyroTurnPIDController getGyroTurnPIDController() {
+        return gyroTurnPIDController;
     }
 
 }
