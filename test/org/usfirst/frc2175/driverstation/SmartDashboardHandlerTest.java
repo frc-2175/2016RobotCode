ceec67eb18224d6662ec0a03117c5ba9c9ee1063
Kevin La Casse <wolfklacasse@gmail.com>
2016-02-20 16:50:47 -0600

    Rebase

--- test/org/usfirst/frc2175/driverstation/SmartDashboardHandlerTest.java
+++ test/org/usfirst/frc2175/driverstation/SmartDashboardHandlerTest.java
@@ -10,7 +10,6 @@ import mockit.Mocked;
 public class SmartDashboardHandlerTest extends TestBase {
     @Mocked
     private RobotSubsystems robotSubsystems;
-
     @Mocked
     private RobotControllers robotControllers;
 
