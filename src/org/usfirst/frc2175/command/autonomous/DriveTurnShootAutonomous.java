cb383350baa39f16efbc350355f124b3ce4d492a
Kevin La Casse <wolfklacasse@gmail.com>
2016-02-21 15:48:43 -0600

    Make general drive turn shoot autonomous

---
+++ src/org/usfirst/frc2175/command/autonomous/DriveTurnShootAutonomous.java
@@ -0,0 +1,36 @@
+package org.usfirst.frc2175.command.autonomous;
+
+import org.usfirst.frc2175.command.single.DriveInches;
+import org.usfirst.frc2175.command.single.ExtendCatapultCommand;
+import org.usfirst.frc2175.command.single.TurnToHeadingCommand;
+import org.usfirst.frc2175.config.AutonomousConfig;
+import org.usfirst.frc2175.config.RobotConfig;
+import org.usfirst.frc2175.pid.RobotControllers;
+import org.usfirst.frc2175.subsystem.RobotSubsystems;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class DriveTurnShootAutonomous extends CommandGroup {
+    public DriveTurnShootAutonomous(RobotSubsystems robotSubsystems,
+            RobotControllers robotControllers) {
+        RobotConfig robotConfig = robotSubsystems.getRobotConfig();
+        AutonomousConfig autonomousConfig = robotConfig.getAutonomousConfig();
+        double travelLength = autonomousConfig.getTravelLength();
+        int distanceAfterTurn = 0;
+        int specifiedDegrees = 0;
+
+        robotSubsystems.getPowertrainSubsystem().resetGyro();
+        addSequential(new DriveInches(robotSubsystems, robotControllers,
+                travelLength));
+        addSequential(new TurnToHeadingCommand(robotSubsystems,
+                robotControllers, 0, true));
+        addSequential(new DriveInches(robotSubsystems, robotControllers,
+                distanceAfterTurn));
+        addSequential(new TurnToHeadingCommand(robotSubsystems,
+                robotControllers, specifiedDegrees, true));
+        // TODO Add auto-aim here
+        addSequential(new ExtendCatapultCommand(robotSubsystems));
+
+    }
+
+}
