b85dd35da9dbb699a8c643b7658dbef0df7dec39
Jeff Jensen <jjensen@apache.org>
2016-02-22 00:47:03 -0600

    Enable ManipulatorSubsystemTest w/partial mock

--- test/org/usfirst/frc2175/subsystem/manipulator/ManipulatorSubsystemTest.java
+++ test/org/usfirst/frc2175/subsystem/manipulator/ManipulatorSubsystemTest.java
@@ -1,6 +1,5 @@
 package org.usfirst.frc2175.subsystem.manipulator;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.usfirst.frc2175.TestBase;
 import org.usfirst.frc2175.config.RobotConfig;
@@ -8,6 +7,7 @@ import org.usfirst.frc2175.config.RobotConfig;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.HLUsageReporting;
+import mockit.Expectations;
 import mockit.Mocked;
 
 public class ManipulatorSubsystemTest extends TestBase {
@@ -23,11 +23,20 @@ public class ManipulatorSubsystemTest extends TestBase {
     @Mocked
     private HLUsageReporting hlUsageReporting;
 
-    @Ignore("WiringConfig needs valid bootSpeed from MockWiringConfig")
     @Test
     public void testManipulatorSubsystem()
             throws IllegalArgumentException, IllegalAccessException {
+        new Expectations() {
+            {
+                // when this is called
+                robotConfig.getManipulatorConfig().getBootSpeed();
+                // then return this result
+                result = 1.0;
+            }
+        };
+
         ManipulatorSubsystem sut = new ManipulatorSubsystem(robotConfig);
+
         assertDoublesNotZero(sut);
         assertInstanceVariablesNotNull(sut);
     }
