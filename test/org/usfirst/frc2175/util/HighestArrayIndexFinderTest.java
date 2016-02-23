4893dec063d3c194936de98b992384aa951a83eb
Jeff Jensen <jjensen@apache.org>
2016-02-22 19:35:09 -0600

    Add test for empty array in index finder

--- test/org/usfirst/frc2175/util/HighestArrayIndexFinderTest.java
+++ test/org/usfirst/frc2175/util/HighestArrayIndexFinderTest.java
@@ -9,7 +9,7 @@ public class HighestArrayIndexFinderTest extends TestBase {
     private HighestArrayIndexFinder sut = new HighestArrayIndexFinder();
 
     @Test
-    public void testDetermineLargestArrayItemIndex() throws Exception {
+    public void testDetermineLargestArrayItemIndex_ManyValues() {
         double[] array = { 123, 1235, 4, 167, 12, 1975, 14 };
         int expectedValue = 5;
 
@@ -18,4 +18,13 @@ public class HighestArrayIndexFinderTest extends TestBase {
         assertEquals("Index does not match!", expectedValue, actualValue);
     }
 
+    @Test
+    public void testDetermineLargestArrayItemIndex_NoValues() {
+        double[] array = {};
+        int expectedValue = HighestArrayIndexFinder.NO_VALUES;
+
+        int actualValue = sut.determineLargestArrayItemIndex(array);
+
+        assertEquals("Index does not match!", expectedValue, actualValue);
+    }
 }
