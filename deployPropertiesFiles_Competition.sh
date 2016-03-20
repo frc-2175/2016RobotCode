db36068d5abfb4e1c00930c71b646db7244124aa
Max Haland <max@haland.org>
2016-03-20 13:31:52 -0500

    Rewrite deploy scripts for speed

--- deployPropertiesFiles_Competition.sh
+++ deployPropertiesFiles_Competition.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 FILES=src/properties/competitionbot/*
+c=""
 for f in $FILES
 do
-	scp $f admin@roborio-2175-frc.local:/home/lvuser
+	c="$c $f"
 done
+scp $c admin@roborio-2175-frc.local:/home/lvuser
 ssh admin@roborio-2175-frc.local "killall -q netconsole-host || :"
--- deployPropertiesFiles_Practice.sh
+++ deployPropertiesFiles_Practice.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 FILES=src/properties/practicebot/*
+c=""
 for f in $FILES
 do
-	scp $f admin@roborio-9001-frc.local:/home/lvuser
+	c="$c $f"
 done
+scp $c admin@roborio-9001-frc.local:/home/lvuser
 ssh admin@roborio-9001-frc.local "killall -q netconsole-host || :"
