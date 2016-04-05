7cc053c023e6af5853f60454b7400f38ecf08e81
Jeff Jensen <jjensen@apache.org>
2016-04-04 19:44:37 -0500

    Minor cleanup; variable shouldn't be static

--- src/org/usfirst/frc2175/subsystem/camera/CameraSubsystem.java
+++ src/org/usfirst/frc2175/subsystem/camera/CameraSubsystem.java
@@ -1,21 +1,25 @@
 package org.usfirst.frc2175.subsystem.camera;
 
+import org.usfirst.frc2175.config.ControlLoopConfig;
 import org.usfirst.frc2175.config.RobotConfig;
 import org.usfirst.frc2175.config.VisionProcessingConfig;
 import org.usfirst.frc2175.subsystem.BaseSubsystem;
 
 public class CameraSubsystem extends BaseSubsystem {
-    private static double CAMERA_FOV;
-    private static double CAMERA_HORIZONTAL_RES;
+    private double cameraFov;
+    private double cameraHorizontalRes;
     private VisionProcessingConfig visionProcessingConfig;
     private double centerCamera;
 
     public CameraSubsystem(RobotConfig robotConfig) {
+        ControlLoopConfig controlLoopConfig =
+                robotConfig.getControlLoopConfig();
+
+        this.centerCamera = controlLoopConfig.getVisionTurnPID_centerCamera();
+
         this.visionProcessingConfig = robotConfig.getVisionProcessingConfig();
-        this.centerCamera = robotConfig.getControlLoopConfig()
-                .getVisionTurnPID_centerCamera();
-        this.CAMERA_FOV = visionProcessingConfig.getCameraFOV();
-        this.CAMERA_HORIZONTAL_RES =
+        this.cameraFov = visionProcessingConfig.getCameraFOV();
+        this.cameraHorizontalRes =
                 visionProcessingConfig.getCameraHorizontalRes();
     }
 
@@ -24,8 +28,7 @@ public class CameraSubsystem extends BaseSubsystem {
     }
 
     public double getGoalDistanceFromCenterInDegrees() {
-        double degreesPerPixel = CAMERA_FOV / CAMERA_HORIZONTAL_RES;
+        double degreesPerPixel = cameraFov / cameraHorizontalRes;
         return getGoalDistanceFromCenterInPixels() * degreesPerPixel;
     }
-
 }
