aeac753c3bbaeaa69ef11ef18b06aa18eb5faebf
Kevin La Casse <wolfklacasse@gmail.com>
2016-04-12 23:19:51 -0500

    Update list of autonomous on the dashboard

--- src/org/usfirst/frc2175/driverstation/AutonomousChooserFactory.java
+++ src/org/usfirst/frc2175/driverstation/AutonomousChooserFactory.java
@@ -1,14 +1,24 @@
 package org.usfirst.frc2175.driverstation;
 
+import org.usfirst.frc2175.command.autonomous.CrossChevalDeFriseAndShootForwardAutonomous;
+import org.usfirst.frc2175.command.autonomous.CrossChevalDeFriseAndShootSlightLeftAutonomous;
+import org.usfirst.frc2175.command.autonomous.CrossChevalDeFriseAndShootSlightRightAutonomous;
+import org.usfirst.frc2175.command.autonomous.CrossChevalDeFriseAutonomous;
+import org.usfirst.frc2175.command.autonomous.CrossPortcullisAndShootForwardAutonomous;
+import org.usfirst.frc2175.command.autonomous.CrossPortcullisAndShootSlightLeftAutonomous;
+import org.usfirst.frc2175.command.autonomous.CrossPortcullisAndShootSlightRightAutonomous;
+import org.usfirst.frc2175.command.autonomous.CrossPortcullisAutonomous;
 import org.usfirst.frc2175.command.autonomous.CrossStaticDefenseAndShootForwardAutonomous;
 import org.usfirst.frc2175.command.autonomous.CrossStaticDefenseAndShootSlightLeftAutonomous;
 import org.usfirst.frc2175.command.autonomous.CrossStaticDefenseAndShootSlightRightAutonomous;
 import org.usfirst.frc2175.command.autonomous.CrossStaticDefenseAutonomous;
-import org.usfirst.frc2175.command.autonomous.DamageSimpleDefenseAutonomous;
+import org.usfirst.frc2175.command.autonomous.CrossStaticDefenseNoEncoderAutonomous;
+import org.usfirst.frc2175.command.autonomous.CrossTwiceChevalDeFriseAutonomous;
+import org.usfirst.frc2175.command.autonomous.CrossTwicePortcullisAutonomous;
+import org.usfirst.frc2175.command.autonomous.CrossTwiceStaticDefenseAutonomous;
 import org.usfirst.frc2175.command.autonomous.DoNothingAutonomous;
 import org.usfirst.frc2175.command.autonomous.TestAllFeaturesAutonomous;
 import org.usfirst.frc2175.command.autonomous.WeakenShootLowBarAutonomous;
-import org.usfirst.frc2175.command.autonomous.CrossStaticDefenseNoEncoderAutonomous;
 import org.usfirst.frc2175.pid.RobotControllers;
 import org.usfirst.frc2175.subsystem.RobotSubsystems;
 
@@ -29,9 +39,9 @@ public class AutonomousChooserFactory {
             RobotControllers robotControllers) {
         autonChooser.addDefault("Do nothing", new DoNothingAutonomous());
 
-        // list weaken ones first as probably most common
-        addWeakenAutons(autonChooser, robotSubsystems, robotControllers);
-        addDamageAutons(autonChooser, robotSubsystems, robotControllers);
+        // list cross ones first as probably most common
+        addCrossAutons(autonChooser, robotSubsystems, robotControllers);
+        addCrossTwiceAutons(autonChooser, robotSubsystems, robotControllers);
 
         autonChooser.addObject("Careful: Test all robot features",
                 new TestAllFeaturesAutonomous(robotSubsystems,
@@ -39,80 +49,71 @@ public class AutonomousChooserFactory {
     }
 
     // WARNING: keep these same order as Damage ones for driver clarity
-    protected void addWeakenAutons(SendableChooser autonChooser,
+    protected void addCrossAutons(SendableChooser autonChooser,
             RobotSubsystems robotSubsystems,
             RobotControllers robotControllers) {
-        autonChooser.addObject("Weaken simple defense and shoot forward",
-                new CrossStaticDefenseAndShootForwardAutonomous(robotSubsystems,
-                        robotControllers));
-        autonChooser.addObject("Weaken simple defense and shoot slight left",
-                new CrossStaticDefenseAndShootSlightLeftAutonomous(
-                        robotSubsystems, robotControllers));
-        autonChooser.addObject("Weaken simple defense and shoot slight right",
-                new CrossStaticDefenseAndShootSlightRightAutonomous(
-                        robotSubsystems, robotControllers));
-        autonChooser.addObject("Weaken simple defense - no encoders",
-                new CrossStaticDefenseNoEncoderAutonomous(robotSubsystems,
-                        robotControllers));
-        autonChooser.addObject("Weaken simple defense",
+        autonChooser.addObject("Cross simple defense",
                 new CrossStaticDefenseAutonomous(robotSubsystems,
                         robotControllers));
-        // autonChooser.addObject("Weaken cheval de frise",
-        // new WeakenChevalDeFriseAutonomous(robotSubsystems,
-        // robotControllers));
-        // autonChooser.addObject("Weaken portcullis",
-        // new WeakenPortcullisAutonomous(robotSubsystems,
-        // robotControllers));
+        autonChooser.addObject("Cross simple defense and shoot forward",
+                new CrossStaticDefenseAndShootForwardAutonomous(robotSubsystems,
+                        robotControllers));
+        autonChooser.addObject("Cross simple defense and shoot slight left",
+                new CrossStaticDefenseAndShootSlightLeftAutonomous(
+                        robotSubsystems, robotControllers));
+        autonChooser.addObject("Cross simple defense and shoot slight right",
+                new CrossStaticDefenseAndShootSlightRightAutonomous(
+                        robotSubsystems, robotControllers));
+        autonChooser.addObject("Cross cheval de frise",
+                new CrossChevalDeFriseAutonomous(robotSubsystems,
+                        robotControllers));
+        autonChooser.addObject("Cross ChevalDeFrise and shoot forward",
+                new CrossChevalDeFriseAndShootForwardAutonomous(robotSubsystems,
+                        robotControllers));
+        autonChooser.addObject("Cross ChevalDeFrise and shoot slight left",
+                new CrossChevalDeFriseAndShootSlightLeftAutonomous(
+                        robotSubsystems, robotControllers));
+        autonChooser.addObject("Cross ChevalDeFrise and shoot slight right",
+                new CrossChevalDeFriseAndShootSlightRightAutonomous(
+                        robotSubsystems, robotControllers));
+        autonChooser.addObject("Cross portcullis",
+                new CrossPortcullisAutonomous(robotSubsystems,
+                        robotControllers));
+        autonChooser.addObject("Cross portcullis and shoot forward",
+                new CrossPortcullisAndShootForwardAutonomous(robotSubsystems,
+                        robotControllers));
+        autonChooser.addObject("Cross portcullis and shoot slight left",
+                new CrossPortcullisAndShootSlightLeftAutonomous(robotSubsystems,
+                        robotControllers));
+        autonChooser.addObject("Cross portcullis and shoot slight right",
+                new CrossPortcullisAndShootSlightRightAutonomous(
+                        robotSubsystems, robotControllers));
+        autonChooser.addObject("Cross simple defense - no encoders",
+                new CrossStaticDefenseNoEncoderAutonomous(robotSubsystems,
+                        robotControllers));
         autonChooser.addObject("Weaken shoot low bar",
                 new WeakenShootLowBarAutonomous(robotSubsystems,
                         robotControllers));
-        // autonChooser.addObject("Weaken shoot cheval de frise",
-        // new WeakenShootChevalDeFriseAutonomous(robotSubsystems,
-        // robotControllers));
-        // autonChooser.addObject("Weaken shoot rough terrain",
-        // new WeakenShootRoughTerrainAutonomous(robotSubsystems,
-        // robotControllers));
-        // autonChooser.addObject("Weaken shoot moat",
-        // new WeakenShootMoatAutonomous(robotSubsystems,
-        // robotControllers));
-        // autonChooser.addObject("Weaken shoot ramparts",
-        // new WeakenShootRampartsAutonomous(robotSubsystems,
-        // robotControllers));
-        // autonChooser.addObject("Weaken shoot porticullis",
-        // new WeakenShootPortcullisAutonomous(robotSubsystems,
-        // robotControllers));
-        // autonChooser.addObject("Weaken shoot rock wall",
-        // new WeakenShootRockWallAutonomous(robotSubsystems,
-        // robotControllers));
     }
 
     // WARNING: keep these same order as Weaken ones for driver clarity
-    protected void addDamageAutons(SendableChooser autonChooser,
+    protected void addCrossTwiceAutons(SendableChooser autonChooser,
             RobotSubsystems robotSubsystems,
             RobotControllers robotControllers) {
-        autonChooser.addObject("Damage simple defense",
-                new DamageSimpleDefenseAutonomous(robotSubsystems,
+        autonChooser.addObject("Cross twice simple defense",
+                new CrossTwiceStaticDefenseAutonomous(robotSubsystems,
                         robotControllers));
-        // autonChooser.addObject("Damage cheval de frise",
-        // new DamageChevalDeFriseAutonomous(robotSubsystems,
-        // robotControllers));
-        // autonChooser.addObject("Damage portcullis",
-        // new DamagePortcullisAutonomous(robotSubsystems,
-        // robotControllers));
+        autonChooser.addObject("Cross twice cheval de frise",
+                new CrossTwiceChevalDeFriseAutonomous(robotSubsystems,
+                        robotControllers));
+        autonChooser.addObject("Cross twice portcullis",
+                new CrossTwicePortcullisAutonomous(robotSubsystems,
+                        robotControllers));
         // autonChooser.addObject("Damage shoot low bar",
         // new DamageShootLowBarAutonomous(robotSubsystems,
         // robotControllers));
         // autonChooser.addObject("Damage shoot cheval de frise",
         // new DamageShootChevalDeFriseAutonomous(robotSubsystems,
-        // robotControllers));
-        // autonChooser.addObject("Damage shoot rough terrain",
-        // new DamageShootRoughTerrainAutonomous(robotSubsystems,
-        // robotControllers));
-        // autonChooser.addObject("Damage shoot moat",
-        // new DamageShootMoatAutonomous(robotSubsystems,
-        // robotControllers));
-        // autonChooser.addObject("Damage shoot ramparts",
-        // new DamageShootRampartsAutonomous(robotSubsystems,
         // robotControllers));
         // autonChooser.addObject("Damage shoot portcullis",
         // new DamageShootPortcullisAutonomous(robotSubsystems,
