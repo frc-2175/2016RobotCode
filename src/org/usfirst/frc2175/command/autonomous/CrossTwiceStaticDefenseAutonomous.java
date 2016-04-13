a41f4754633829aaedb2835d2a0fb8734ba8522a
Kevin La Casse <wolfklacasse@gmail.com>
2016-04-12 22:11:11 -0500

    Make an auton for damaging a defense

---
+++ src/org/usfirst/frc2175/command/autonomous/CrossTwiceStaticDefenseAutonomous.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc2175.command.autonomous;
+
+import org.usfirst.frc2175.command.autonomous.block.DriveUpToOuterworksBlock;
+import org.usfirst.frc2175.command.single.DriveInchesCommand;
+import org.usfirst.frc2175.pid.RobotControllers;
+import org.usfirst.frc2175.subsystem.RobotSubsystems;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class CrossTwiceStaticDefenseAutonomous extends CommandGroup {
+    public CrossTwiceStaticDefenseAutonomous(RobotSubsystems robotSubsystems,
+            RobotControllers robotControllers) {
+        // drive up to outerworks
+        addSequential(new DriveUpToOuterworksBlock(robotSubsystems,
+                robotControllers));
+        // cross static defense
+        addSequential(new DriveInchesCommand(robotSubsystems, robotControllers,
+                111.4));
+        // cross static defense
+        addSequential(new DriveInchesCommand(robotSubsystems, robotControllers,
+                -111.4));
+        // cross static defense
+        addSequential(new DriveInchesCommand(robotSubsystems, robotControllers,
+                81.4));
+    }
+}
