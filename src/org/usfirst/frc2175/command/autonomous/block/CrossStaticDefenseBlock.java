d5526c90243bf3e7a3893740be303d8b037cd3c6
Max Haland <max@haland.org>
2016-04-17 18:26:49 -0500

    Increase drive amount

--- src/org/usfirst/frc2175/command/autonomous/block/CrossStaticDefenseBlock.java
+++ src/org/usfirst/frc2175/command/autonomous/block/CrossStaticDefenseBlock.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class CrossStaticDefenseBlock extends CommandGroup {
     public CrossStaticDefenseBlock(RobotSubsystems robotSubsystems,
             RobotControllers robotControllers, boolean goingForwards) {
-        double moveAmount = 135;
+        double moveAmount = 145;
         if (!goingForwards) {
             moveAmount = -moveAmount;
         }
