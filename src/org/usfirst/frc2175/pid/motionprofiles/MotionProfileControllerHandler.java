f1f2a3652f53e54bdc78bda8cef6fc78c9a29cd7
Max Haland <max@haland.org>
2016-04-20 13:47:05 -0500

    Add more Javadoc

--- src/org/usfirst/frc2175/pid/motionprofiles/MotionProfileControllerHandler.java
+++ src/org/usfirst/frc2175/pid/motionprofiles/MotionProfileControllerHandler.java
@@ -5,6 +5,13 @@ import java.util.TimerTask;
 
 import org.usfirst.frc2175.pid.PIDControllerComplete;
 
+/**
+ * The MotionProfileControllerHandler class is designed to execute
+ * {@link #MotionProfile MotionProfiles}.
+ *
+ * @author Max Haland
+ *
+ */
 public class MotionProfileControllerHandler {
 
     private Timer setpointSetterTimer;
@@ -13,19 +20,52 @@ public class MotionProfileControllerHandler {
     private boolean hasProfileStarted = false;
     private boolean isProfileFinished = false;
 
+    /**
+     * Constructor for a new {@link MotionProfileControllerHandler} with a
+     * specified {@link MotionProfile} and an array of controllers
+     * {@link #PIDControllerComplete PIDControllerComplete[]}.
+     *
+     * @param profile
+     *            MotionProfile to execute
+     * @param controllers
+     *            Array of {@link #PIDControllerComplete PIDControllerCompletes}
+     *            to execute with
+     */
     public MotionProfileControllerHandler(MotionProfile profile,
             PIDControllerComplete... controllers) {
         this.profile = profile;
         this.controllers = controllers;
     }
 
-    private void updateControllerSetpoints(int time) {
-        for (PIDControllerComplete controller : controllers) {
-            double setpoint = profile.getMotionProfilePointAtTime(time).getVelocity();
-            controller.setSetpoint(setpoint);
+    /**
+     * Updates the setpoint of each {@link PIDControllerComplete} based on a
+     * timestamp for a {@link MotionProfilePoint}. The point is retrieved from
+     * the {@link MotionProfile} based on the specified timestamp.
+     *
+     * @param timestamp
+     *            Timestamp for MotionProfilePoint. Must be a multiple of dTime
+     *            for the controller.
+     */
+    private void updateControllerSetpoints(int timestamp) {
+        if (timestamp % profile.getDTime() != 0) {
+            throw new IllegalArgumentException(
+                    "Timestamp must be a multiple of dTime!");
+        } else {
+            for (PIDControllerComplete controller : controllers) {
+                double setpoint = profile.getMotionProfilePointAtTime(timestamp)
+                        .getVelocity();
+                controller.setSetpoint(setpoint);
+            }
         }
     }
 
+    /**
+     * Starts the handler. A new {@link Timer} is created, and a new
+     * {@link ControllerHandlerTimerTask} with the necessary
+     * {@link MotionProfile} is scheduled on a loop. The frequency of the loop
+     * is the same as dTime for the MotionProfile. Finally, the PID controllers
+     * are enabled.
+     */
     public void enable() {
         ControllerHandlerTimerTask controllerTask =
                 new ControllerHandlerTimerTask(profile);
@@ -35,6 +75,10 @@ public class MotionProfileControllerHandler {
         hasProfileStarted = true;
     }
 
+    /**
+     * Stops the controllers and cancels the timer. This is called automatically
+     * when the profile is finished.
+     */
     public void disable() {
         setpointSetterTimer.cancel();
         disableControllers();
@@ -60,9 +104,27 @@ public class MotionProfileControllerHandler {
         return isProfileFinished;
     }
 
+    /**
+     * Framework for running the profile update methods on a {@link Timer}. The
+     * task runs exactly once for each {@link MotionProfilePoint} in the
+     * {@link MotionProfile}, then terminates itself.
+     *
+     * @author Max Haland
+     *
+     *
+     */
     public class ControllerHandlerTimerTask extends TimerTask {
+        /**
+         * Represents whether the task has started.
+         */
         private boolean hasStarted = false;
+        /**
+         * Represents whether the task is finished.
+         */
         private boolean isFinished = false;
+        /**
+         * Timestamp of point to run.
+         */
         private int currentTime = 0;
 
         private int dTime;
@@ -71,6 +133,11 @@ public class MotionProfileControllerHandler {
             this.dTime = profile.getDTime();
         }
 
+        /**
+         * Increases the value of {@link #currentTime} based on the dTime value
+         * for a {@link MotionProfile}. Once currentTime is equal to the total
+         * time to run the profile, {@link #isFinished} is set to true.
+         */
         private void indexTime() {
             if (!hasStarted) {
                 hasStarted = !hasStarted;
@@ -83,13 +150,20 @@ public class MotionProfileControllerHandler {
             }
         }
 
+        /**
+         * Updates the controller setpoints with
+         * {@link #MotionProfileControlHandler.updateControllerSetpoints()},
+         * then calls {@link #indexTime()} to prepare for the next run. If after
+         * the time is indexed, {@link #isFinished} is true, the task and its
+         * timer are cancelled, and the controllers are disabled.
+         */
         @Override
         public void run() {
             updateControllerSetpoints(currentTime);
             indexTime();
             if (isFinished) {
                 cancel();
-                setpointSetterTimer.cancel();
+                disable();
             }
         }
 
