116a83d8862907e45af3bb8b5d244b341db4f74b
Max Haland <max@haland.org>
2016-04-20 09:13:28 -0500

    Add motion profile controllers to RobotControllers

--- src/org/usfirst/frc2175/pid/RobotControllers.java
+++ src/org/usfirst/frc2175/pid/RobotControllers.java
@@ -4,6 +4,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.usfirst.frc2175.config.RobotConfig;
+import org.usfirst.frc2175.pid.motionprofiles.MotionProfileDrivePIDController_Left;
+import org.usfirst.frc2175.pid.motionprofiles.MotionProfileDrivePIDController_Right;
 import org.usfirst.frc2175.subsystem.RobotSubsystems;
 import org.usfirst.frc2175.subsystem.vision.VisionProcessing;
 
@@ -15,6 +17,9 @@ public class RobotControllers {
     private final VisionTurnPIDController visionTurnPIDController;
     private final GyroTurnPIDController gyroTurnPIDController;
     private final DriveInchesPIDController driveInchesPIDController;
+
+    private final MotionProfileDrivePIDController_Left motionProfileDrivePIDController_Left;
+    private final MotionProfileDrivePIDController_Right motionProfileDrivePIDController_Right;
 
     public RobotControllers(RobotSubsystems robotSubsystems,
             RobotConfig robotConfig, VisionProcessing visionProcessing) {
@@ -28,6 +33,13 @@ public class RobotControllers {
                     new GyroTurnPIDController(robotSubsystems, robotConfig);
             this.driveInchesPIDController =
                     new DriveInchesPIDController(robotSubsystems, robotConfig);
+
+            this.motionProfileDrivePIDController_Left =
+                    new MotionProfileDrivePIDController_Left(robotSubsystems,
+                            robotConfig);
+            this.motionProfileDrivePIDController_Right =
+                    new MotionProfileDrivePIDController_Right(robotSubsystems,
+                            robotConfig);
         } catch (Exception e) {
             final String msg =
                     "Error instantiating a PIDController in RobotControllers:";
@@ -47,4 +59,13 @@ public class RobotControllers {
     public DriveInchesPIDController getDriveInchesPIDController() {
         return driveInchesPIDController;
     }
+
+    public MotionProfileDrivePIDController_Left getMotionProfileDrivePIDController_Left() {
+        return motionProfileDrivePIDController_Left;
+    }
+
+    public MotionProfileDrivePIDController_Right getMotionProfileDrivePIDController_Right() {
+        return motionProfileDrivePIDController_Right;
+    }
+
 }
