217eee7e7bdb356746f1442a2bd0d02c69f5027b
Max Haland <max@haland.org>
2016-04-20 10:43:22 -0500

    Add javadoc

--- src/org/usfirst/frc2175/command/single/DriveDistanceProfileCommand.java
+++ src/org/usfirst/frc2175/command/single/DriveDistanceProfileCommand.java
@@ -7,9 +7,31 @@ import org.usfirst.frc2175.pid.motionprofiles.MotionProfileControllerHandler;
 import org.usfirst.frc2175.pid.motionprofiles.MotionProfiler;
 import org.usfirst.frc2175.pid.motionprofiles.MotionProfiler.MotionProfile;
 
+/**
+ * Command to drive a specific distance using a generated motion profile. The
+ * profile is generated based on arguments passed to the constructor, and then
+ * executed.
+ *
+ * @author Max Haland
+ *
+ */
 public class DriveDistanceProfileCommand extends BaseCommand {
     private MotionProfileControllerHandler profileHandler;
 
+    /**
+     * Constructor to generate a profile based on a distance, maximum velocity,
+     * maximum acceleration, and time interval between each step. The profile is
+     * generated, then fed to a profile handler.
+     *
+     * @param distance
+     *            Total distance to drive
+     * @param maxVelocity
+     *            Maximum velocity allowed
+     * @param maxAcceleration
+     *            Maximum acceleration allowed
+     * @param dTime
+     *            Time interval between each discrete step
+     */
     public DriveDistanceProfileCommand(RobotConfig robotConfig,
             RobotControllers robotControllers, double distance,
             double maxVelocity, double maxAcceleration, int dTime) {
@@ -30,7 +52,8 @@ public class DriveDistanceProfileCommand extends BaseCommand {
 
     @Override
     protected void execute() {
-
+        // PID controllers handle themselves, so we don't need to do anything
+        // here.
     }
 
     @Override
