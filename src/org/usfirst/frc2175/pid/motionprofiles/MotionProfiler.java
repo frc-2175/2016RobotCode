a3237d7b2c819752468da1f7b8fe47de96d5c415
Max Haland <max@haland.org>
2016-04-20 13:07:37 -0500

    Add method to generate profile with default time

--- src/org/usfirst/frc2175/pid/motionprofiles/MotionProfiler.java
+++ src/org/usfirst/frc2175/pid/motionprofiles/MotionProfiler.java
@@ -3,7 +3,7 @@ package org.usfirst.frc2175.pid.motionprofiles;
 public class MotionProfiler {
 
     /**
-     * Generates a trapezoidal velocity motion profile.
+     * Generates a trapezoidal velocity motion profile with a specific dTime
      *
      * @param distance
      *            Distance to drive
@@ -31,6 +31,53 @@ public class MotionProfiler {
         // Loop to add points to profile
         for (int time = 0; time <= 2 * t_accel + t_constantSpeed; time =
                 time + dTime) {
+            MotionProfilePoint point;
+            // Handle each case separately: acceleration, constant speed,
+            // deceleration
+            if (time < t_accel) {
+                double velocityValue = maxAcceleration * time;
+                point = new MotionProfilePoint(time, velocityValue);
+            } else if (t_accel <= time && time < t_accel + t_constantSpeed) {
+                point = new MotionProfilePoint(time, maxVelocity);
+            } else {
+                double velocityValue =
+                        maxAcceleration * (time - t_accel - t_constantSpeed);
+                point = new MotionProfilePoint(time, velocityValue);
+            }
+            // Add the point to the profile
+            profile.addMotionProfilePoint(point);
+        }
+        return profile;
+    }
+
+    /**
+     * Generates a trapezoidal velocity motion profile with default dTime.
+     *
+     * @param distance
+     *            Distance to drive
+     * @param maxVelocity
+     *            Highest speed allowed
+     * @param maxAcceleration
+     *            Highest acceleration allowed
+     *
+     */
+    /*
+     * TODO make unit tests for this (or test it with a spreadsheet or
+     * something). Currently, it probably doesn't work for distances where the
+     * robot cannot hit max velocity.
+     */
+    public MotionProfile generateDriveDistanceMotionProfile(double distance,
+            double maxVelocity, double maxAcceleration) {
+        MotionProfile profile = new MotionProfile();
+
+        // Values for profile
+        double t_accel = (maxVelocity / maxAcceleration);
+        double t_constantSpeed = Math.sqrt(
+                (distance / maxVelocity) - (maxVelocity / maxAcceleration));
+
+        // Loop to add points to profile
+        for (int time = 0; time <= 2 * t_accel + t_constantSpeed; time =
+                time + MotionProfile.DEFAULT_D_TIME) {
             MotionProfilePoint point;
             // Handle each case separately: acceleration, constant speed,
             // deceleration
