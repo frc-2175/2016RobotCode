339ae8191dd194eb0533e318a6cd393980bc8507
Jacob Jensen <jjja380@gmail.com>
2016-04-23 20:16:29 -0500

    Reduce distance auton driven past defense
    Better shot, less distance to drive back for drivers.

--- src/org/usfirst/frc2175/command/autonomous/block/CrossStaticDefenseBlock.java
+++ src/org/usfirst/frc2175/command/autonomous/block/CrossStaticDefenseBlock.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class CrossStaticDefenseBlock extends CommandGroup {
     public CrossStaticDefenseBlock(RobotSubsystems robotSubsystems,
             RobotControllers robotControllers, boolean goingForwards) {
-        double moveAmount = 150;
+        double moveAmount = 12 * 9 + 4;
         if (!goingForwards) {
             moveAmount = -moveAmount;
         }
