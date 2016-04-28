635cf8d0df55373cf2ae6ae44fc7e1492d16fffc
Max Haland <max@haland.org>
2016-04-28 12:21:01 -0500

    Tweak distance

--- src/org/usfirst/frc2175/command/autonomous/block/CrossStaticDefenseBlock.java
+++ src/org/usfirst/frc2175/command/autonomous/block/CrossStaticDefenseBlock.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class CrossStaticDefenseBlock extends CommandGroup {
     public CrossStaticDefenseBlock(RobotSubsystems robotSubsystems,
             RobotControllers robotControllers, boolean goingForwards) {
-        double moveAmount = 12 * 11;
+        double moveAmount = 12 * 12;
         if (!goingForwards) {
             moveAmount = -moveAmount;
         }
